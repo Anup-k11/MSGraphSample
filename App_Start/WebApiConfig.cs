79568bf2dbdc634faa7f079ea93c0af6ba446449
Anup Kishore <gullumahato72@gmail.com>
2022-07-11 11:07:32 +0530

    initial commit

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web;
+
+namespace sample.App_Start
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Ignore AAD Auth for WebAPI...will be handled by WopiTokenValidationFilter class
+            config.SuppressDefaultHostAuthentication();
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "WopiDefault",
+                routeTemplate: "wopi/{controller}/{id}/{action}",
+                defaults: new { id = RouteParameter.Optional, action = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Models/Wopi/WopiRequestType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample.Models.Wopi
+{
+    public enum WopiRequestType
+    {
+        None,
+        CheckFileInfo,
+        GetFile,
+        Lock,
+        GetLock,
+        RefreshLock,
+        Unlock,
+        UnlockAndRelock,
+        PutFile,
+        PutRelativeFile,
+        RenameFile,
+        PutUserInfo
+    }
+}
